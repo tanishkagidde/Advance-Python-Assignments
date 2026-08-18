d3e819df5277ad44b8d81d98c971f5797dea9fd8
tanishkagidde <tanishka.gidde@gmail.com>
2026-08-18 12:49:47 +0530

    Implement longest common substring program
    Added a Python program to find the longest common substring using dynamic programming.

---
+++ Scenario 2/Question8.py
@@ -0,0 +1,10 @@
+	'''
+  8. Longest Common Substring									
+										
+	Develop a Python program to determine the length of the longest common substring between two strings.									
+										
+	Requirements									
+		Accept two strings.								
+		Use Dynamic Programming.								
+		Display the length of the longest common substring.					
+'''
